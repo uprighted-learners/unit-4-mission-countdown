b476603a18037ce3694ba7021aad4be5133994b8
Ben Anderson <andersjbe@gmail.com>
2024-03-05 11:29:46 -1000

    Completed part 2

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const startTimeout = document.getElementById("start-timeout");
 const timeoutDisplay = document.getElementById("timeout-display");
 const startInterval = document.getElementById("start-interval");
 const intervalDisplay = document.getElementById("interval-display");
+const repeatGo = document.querySelectorAll(".repeat-go");
 
 startTimeout.addEventListener("click", () => {
   timeoutDisplay.textContent = "Getting ready...";
@@ -19,6 +20,17 @@ startInterval.addEventListener("click", () => {
     if (count < 0) {
       intervalDisplay.textContent = "GO";
       clearInterval(myInterval);
+
+      const repeatGoArray = Array.from(repeatGo);
+      let index = 0;
+      const newInterval = setInterval(() => {
+        repeatGoArray[index].textContent = "GO!";
+        index += 1;
+
+        if (repeatGoArray[index] === undefined) {
+          clearInterval(newInterval);
+        }
+      }, 1000);
     }
   }, 1000);
 });
