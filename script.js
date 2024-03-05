4e212f77367f90e650b72ca18758099f07dd9b98
Ben Anderson <andersjbe@gmail.com>
2024-03-05 11:10:38 -1000

    Completed part 1

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-// Your code goes here
+const startTimeout = document.getElementById("start-timeout");
+const timeoutDisplay = document.getElementById("timeout-display");
+const startInterval = document.getElementById("start-interval");
+const intervalDisplay = document.getElementById("interval-display");
+
+startTimeout.addEventListener("click", () => {
+  timeoutDisplay.textContent = "Getting ready...";
+  setTimeout(() => {
+    timeoutDisplay.textContent = "READY";
+  }, 5000);
+});
+
+startInterval.addEventListener("click", () => {
+  let count = 5;
+  let myInterval = setInterval(() => {
+    intervalDisplay.textContent = count;
+    count--;
+
+    if (count < 0) {
+      intervalDisplay.textContent = "GO";
+      clearInterval(myInterval);
+    }
+  }, 1000);
+});
